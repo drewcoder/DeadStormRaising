4078f60a830c01265c24531fd81d7652e87c3e14
drewcoder <drewcoder@gmail.com>
2021-12-05 05:49:47 +0900

    Initial Commit

---
+++ DeadStormRaising tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  DeadStormRaising tvOS
+//
+//  Created by Ritchie Andrews on 12/5/21.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
